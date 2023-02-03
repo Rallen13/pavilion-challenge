f4739b5faf75a42269f07f7078ec6dd760588091
Rachel Allen <rallenco13@gmail.com>
2023-02-02 19:49:20 -0700

    saving prev link and next link for button components to come

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ import MemberCardContainer from './components/MemberCardContainer/MemberCardCont
 
 function App() {
   const [members, setMembers] = useState([]);
+  const [prevPage, setPrevPage] = useState('');
+  const [nextPage, setNextPage] = useState('');
 
   useEffect(() => {
     try {
       getMembers().then((response) => {
         setMembers(response.data);
-      })
+        setPrevPage(response.url);
+        const nextLink = splitNextLink(response.headers.link);
+        setNextPage(nextLink);
+      });
     } catch (error) {
       console.log(error);
     }
   }, []);
 
+  const splitNextLink = (link) => {
+    return link.split(', ')[0].split('>')[0].slice(1);
+  };
+
   return (
     <div className="App">
-      <img src="https://back.joinpavilion.com/wp-content/uploads/2022/09/pavilion-logo.svg" alt="" class="jsx-2827836672"></img>
+      <img
+        src="https://back.joinpavilion.com/wp-content/uploads/2022/09/pavilion-logo.svg"
+        alt=""
+      ></img>
       <MemberCardContainer members={members} />
+      <div>
+      </div>
     </div>
   );
 }
